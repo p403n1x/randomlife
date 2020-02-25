e4a1167bbb72682d2a27944601065cb62cad53e3
olivier bouzon <oliv@hotmail.fr>
2020-02-25 14:47:03 +0100

    Associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :groupcategories
+  has_many :options, dependent: :destroy
+  has_many :tasks
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_many :groupcategories, dependent: :destroy
+  has_many :users
 end

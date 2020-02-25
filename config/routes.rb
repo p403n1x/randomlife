60d669a069fba885591144df590c6ac4e84f5044
Noemie Fournier <noemie_fournier@orange.fr>
2020-02-25 16:58:33 +0100

    controller tasks done

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'options/index'
-  get 'options/show'
-  get 'options/create'
-  get 'options/update'
-  get 'options/destroy'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'assigned_tasks/edit'
-  get 'assigned_tasks/update'
-  get 'users/show'
   devise_for :users
   root to: 'pages#home'
 

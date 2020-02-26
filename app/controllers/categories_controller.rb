dbe4e851741bfe67cb0fd1db187675b8302ee312
Noemie Fournier <noemie_fournier@orange.fr>
2020-02-26 11:50:42 +0100

    en cours

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  skip_before_action :authenticate_user!
+  #skip_before_action :authenticate_user!
   def index
     @categories = Category.all
   end

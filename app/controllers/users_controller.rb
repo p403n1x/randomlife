2097f19b5fb34d9ad9937d325613a36e727433f1
anaelle claude <anaelleclaude@hotmail.fr>
2020-02-27 14:39:12 +0100

    user profil pas trop mal pour l'instant hehe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
   #skip_before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update]
+
   def show
     @tasks = current_user.tasks
     @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :nickname, :email, :phone_number, :image)
   end
 end
 

7e7e104441c30771228a20ca30b2059d1cbbfed0
anaelle claude <anaelleclaude@hotmail.fr>
2020-02-27 14:07:14 +0100

    display profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   #skip_before_action :authenticate_user!
   def show
     @tasks = current_user.tasks
-    #@user.id = current_user
-
     @user = current_user
   end
 end

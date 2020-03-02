4eeeaf4d57195a483c65c809a240b8a497f2bb93
anaelle claude <anaelleclaude@hotmail.fr>
2020-03-02 18:56:43 +0100

    delete category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_many :groupcategories, dependent: :destroy
   has_many :groups, through: :groupcategories
   has_many :options, dependent: :destroy
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_one_attached :photo
   validates :title, presence: true, uniqueness: true
   validates :kind, inclusion: { in: ["Task", "Event"] }

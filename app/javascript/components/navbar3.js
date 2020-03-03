d5753bbf0c7e7ac8994942807b331d6393aaf9e9
anaelle claude <anaelleclaude@hotmail.fr>
2020-03-03 19:16:48 +0100

    nav bar solid en cours

--- app/javascript/components/navbar3.js
+++ app/javascript/components/navbar3.js
@@ -1,6 +1,17 @@
 var navTrigger = document.getElementsByClassName('nav-trigger')[0];
 var body = document.getElementsByTagName('body')[0];
 
+const navbar = document.querySelector('.intro');
+
 navTrigger.addEventListener('click', event => {
   body.classList.toggle('nav-open');
+});
+
+
+window.addEventListener('scroll', event => {
+  if (window.scrollY >= 1%) {
+    navbar.classList.add('intro-solid');
+  } else {
+    navbar.classList.remove('intro-solid');
+  }
 });

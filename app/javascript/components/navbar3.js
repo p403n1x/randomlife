c1c3ca1c735c11a7a2a150bb9d7fe653a3c40d46
anaelle claude <anaelleclaude@hotmail.fr>
2020-03-04 11:56:16 +0100

    navbar majestueuse done

--- app/javascript/components/navbar3.js
+++ app/javascript/components/navbar3.js
@@ -1,17 +1,25 @@
 var navTrigger = document.getElementsByClassName('nav-trigger')[0];
 var body = document.getElementsByTagName('body')[0];
-
-const navbar = document.querySelector('.intro');
+var main = document.querySelector('main');
 
 navTrigger.addEventListener('click', event => {
   body.classList.toggle('nav-open');
 });
 
 
-window.addEventListener('scroll', event => {
-  if (window.scrollY >= 1%) {
+main.addEventListener('scroll', event => {
+  const navbar = document.querySelector('.intro');
+  if (main.scrollTop >= 22) {
     navbar.classList.add('intro-solid');
-  } else {
+  }
+  else {
     navbar.classList.remove('intro-solid');
   }
 });
+
+
+// if (window.scrollY >= window.innerHeight) {
+//         navbar.classList.add('navbar-lewagon-white');
+//       } else {
+//         navbar.classList.remove('navbar-lewagon-white');
+//       }

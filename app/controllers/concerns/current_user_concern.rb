0091790fa347dcbff593424862e42285df462302
olivier bouzon <oliv@hotmail.fr>
2020-03-04 09:28:40 +0100

    end of input crash heroku

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,9 +1,9 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
 
-  included do
-    before_action :set_current_user
-  end
+  # included do
+  #   before_action :set_current_user
+  # end
 
   def set_current_user
     if session[:user_id]

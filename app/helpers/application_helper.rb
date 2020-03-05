c979e56cf99db13ee5bf31978047d9b402aa7a2e
anaelle claude <anaelleclaude@hotmail.fr>
2020-03-05 18:41:27 +0100

    img par defaut user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def default_photo_tag(user, attrs)
+    if user.photo.attached?
+      cl_image_tag user.photo.key, attrs
+    else
+      image_tag "frog.jpg", attrs
+    end
+  end
+
 end
